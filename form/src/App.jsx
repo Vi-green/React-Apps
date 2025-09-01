695877a6df3f6992b81d1728bc8fba5582483c96
Vic-green <victoriagreenco@gmail.com>
2025-09-01 14:50:45 -0300

    boton de add calendar 	modified:   form/package-lock.json 	modified:   form/package.json 	modified:   form/src/App.jsx 	modified:   form/src/Form.jsx 	modified:   form/src/SelectDate.jsx 	modified:   form/src/SelectTime.jsx 	modified:   form/src/index.css

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -4,16 +4,23 @@ import Header from './Header.jsx'
 import Form from './Form.jsx'
 import FormC from './FormC.jsx'
 import Logo from './Logo.jsx'
+import { AddToCalendarButton } from 'add-to-calendar-button-react';
+
+
 
 const App = () => {
   const [conditional, setConditional] = useState(true);
   const [formData, setFormData] = useState(null)
   const [val, setVal] = useState(null)
+  const [hora, setHora] = useState("")
+
 
 
   const handleFormSubmit = (data) => {
     setFormData(data);
+    setHora(data.hora.slice(0,5));
     setConditional(false);
+    
   }
 
   const handleClick = (e) => {
@@ -55,14 +62,32 @@ const App = () => {
       </h1>
       <h3>&#9989; Día: {formData.dia} </h3>
       <p></p>
-      <h3>&#9989; Hora: {formData.hora} </h3>
+      <h3>&#9989; Hora: {hora.substring(0,5)} </h3> 
       <p></p>
       <h3>&#9989; Depósito: {val == "true" ? "4 de Febrero 3640 - San Martín" : "Sargento Cabral 1130 - San Martín"}  </h3>
       <p></p>
       <h4>Muchas gracias por usar nuestro sistema. </h4>
       <p></p>
       <h4>En caso de querer cancelar su turno, por favor comunicarse con nuestro departamento de Compras.</h4>
-      <button onClick={handleClick} value={(true)}> Sacar otro turno </button>
+
+      <nav className="container">
+      
+      <div className="item"><button onClick={handleClick} value={(true)}> Sacar otro turno </button> </div> 
+      
+      <div className="item"><AddToCalendarButton 
+      name="Turno - Greenco"
+      startDate={`${formData.dia}`}  
+      styleLight="--btn-background: #28ad0d; --btn-text: #fff; --font: system-ui, Avenir, Helvetica, Arial, sans-serif;"
+      styleDark="--btn-background: #000;"
+      options={['Apple','Google','Yahoo','Microsoft365', 'MicrosoftTeams', 'Outlook.com']} 
+      label="Agregar al Calendario">
+      </AddToCalendarButton></div> 
+      
+      
+      </nav>
+      <p></p>
+           
+
       <Logo />
     </div>
   )
