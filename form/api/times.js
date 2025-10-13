a4817ce1233bd0d4eaef238e112386ca60213281
Vic-green <victoriagreenco@gmail.com>
2025-10-13 08:49:29 -0300

    AHHHH 	modified:   form/api/times.js 	modified:   form/api/timesC.js

--- form/api/times.js
+++ form/api/times.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   }
 
   try {
-       const param = req.query.formattedParam;
+       const param = req.query.param;
 
     if (!param) {
       return res.status(400).json({ error: 'param is required' });
